267dac80c631e3309a67e34041b74cee9c1e732b
Guilherme Roberti <guimourinha@gmail.com>
2022-02-19 18:17:19 -0300

    post component version 1.0

--- src/routes/Feed.js
+++ src/routes/Feed.js
@@ -1,13 +1,35 @@
-import Layout from "../components/Layout";
 import Post from "../components/Post";
 
 const Feed = () => {
   return (
     <div className="">
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        image={"/img/avatar/dogo.jpg"}
+        name={"Dogo, o Doguinho"}
+        url={"@dogo_doguinho"}
+        time={"2 min"}
+        post={
+          "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta explicabo, error at debitis rem magnam praesentium minima quibusdam tempore officiis, aperiam neque autem. Eaque doloremque dicta repudiandae, non commodi cumque!"
+        }
+      />
+      <Post
+        image={"/img/avatar/dogo.jpg"}
+        name={"Dogo, o Doguinho"}
+        url={"@dogo_doguinho"}
+        time={"2 min"}
+        post={
+          "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta explicabo, error at debitis rem magnam praesentium minima quibusdam tempore officiis, aperiam neque autem. Eaque doloremque dicta repudiandae, non commodi cumque!"
+        }
+      />
+      <Post
+        image={"/img/avatar/dogo.jpg"}
+        name={"Dogo, o Doguinho"}
+        url={"@dogo_doguinho"}
+        time={"2 min"}
+        post={
+          "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta explicabo, error at debitis rem magnam praesentium minima quibusdam tempore officiis, aperiam neque autem. Eaque doloremque dicta repudiandae, non commodi cumque!"
+        }
+      />
     </div>
   );
 };
