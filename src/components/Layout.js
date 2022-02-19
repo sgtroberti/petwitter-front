20725aa8be795875bf8993ad611f50d47f3ae6cb
Guilherme Roberti <guimourinha@gmail.com>
2022-02-19 18:56:02 -0300

    Layout version 1

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,21 @@
 import { Link, Outlet } from "react-router-dom";
-import { Box, Button, Image } from "@chakra-ui/react";
+import { Box, Button, Image, Flex } from "@chakra-ui/react";
+import { useState } from "react";
 
 import AuthStatus from "./AuthStatus";
 
 function Layout() {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <div>
       <Box
         h="48px"
         display="flex"
         boxShadow="0px 2px 4px rgba(33, 33, 33, 0.2)"
+        backgroundColor="white"
+        position="fixed"
+        w="100%"
       >
         <Button
           _active={{ backgroundColor: "none" }}
@@ -38,8 +44,35 @@ function Layout() {
           <Link to="/protected">Protected Page</Link>
         </li>
       </ul> */}
-
+      <Box pb="48px" />
       <Outlet />
+      <Flex
+        position="fixed"
+        top="calc(100vh - 64px)"
+        alignItems="center"
+        justifyContent="center"
+        h="64px"
+        w="100%"
+        border="1px solid #EEEEEE"
+        backgroundColor="white"
+      >
+        <Image
+          src={isLoading ? "img/loading.svg" : null}
+          alt="loading"
+          w="35px"
+          h="35px"
+        />
+        <Image
+          position="fixed"
+          left="calc(100% - 72px)"
+          top="calc(100vh - 78px)"
+          src="img/addPost.png"
+          w="56px"
+          h="56px"
+          alt="addPost"
+          zIndex="2"
+        />
+      </Flex>
     </div>
   );
 }
--- src/routes/Feed.js
+++ src/routes/Feed.js
@@ -30,6 +30,24 @@ const Feed = () => {
           "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta explicabo, error at debitis rem magnam praesentium minima quibusdam tempore officiis, aperiam neque autem. Eaque doloremque dicta repudiandae, non commodi cumque!"
         }
       />
+      <Post
+        image={"/img/avatar/dogo.jpg"}
+        name={"Dogo, o Doguinho"}
+        url={"@dogo_doguinho"}
+        time={"2 min"}
+        post={
+          "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta explicabo, error at debitis rem magnam praesentium minima quibusdam tempore officiis, aperiam neque autem. Eaque doloremque dicta repudiandae, non commodi cumque!"
+        }
+      />
+      <Post
+        image={"/img/avatar/dogo.jpg"}
+        name={"Dogo, o Doguinho"}
+        url={"@dogo_doguinho"}
+        time={"2 min"}
+        post={
+          "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta explicabo, error at debitis rem magnam praesentium minima quibusdam tempore officiis, aperiam neque autem. Eaque doloremque dicta repudiandae, non commodi cumque!"
+        }
+      />
     </div>
   );
 };
